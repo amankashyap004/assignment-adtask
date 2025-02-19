43639ff35952e6db7b981fda4732a6b39f66ac49
amankashyap004 <amankashyap004@gmail.com>
2025-02-19 13:42:14 +0530

    update: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import HeroSection from "@/components/home/HeroSection";
 import Layout from "@/components/layout/Layout";
+import HeroSection from "@/components/home/HeroSection";
+import VideoSection from "@/components/home/VideoSection";
+import Features from "@/components/home/Features";
+import Services from "@/components/home/Services";
+import Testimonial from "@/components/home/Testimonial";
+import ContactUs from "@/components/home/ContactUs";
+import DigitalPresence from "@/components/home/DigitalPresence";
+import ChatBot from "@/components/ChatBot";
 
 export default function Home() {
   return (
     <Layout>
-      <main className="flex flex-col gap-16 lg:gap-20 w-full">
-        <HeroSection />
-        <div />
+      <main className="relative flex flex-col gap-16 md:gap-24 lg:gap-32 w-full">
+        <div>
+          <HeroSection />
+          <VideoSection />
+        </div>
+        <Features />
+        <Services />
+        <Testimonial />
+        <ContactUs />
+        <DigitalPresence />
+        <ChatBot />
       </main>
     </Layout>
   );
